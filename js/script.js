7f996351314895956d0017596247d80e30677c09
Matthew <matt-hope@hotmail.com>
2020-09-02 00:12:49 -0400

    started array function

--- js/script.js
+++ js/script.js
@@ -10,45 +10,82 @@ class Array {
 
     push(value){
         this.data.push(value);
+        this.length++;
+    }
+    
+    delete(index) {
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
     }
 
     display(){
-        console.log("data: ");
         console.log(this.data);
     }
 
 }
 
+
 let p = "p";
 
 $(document).ready(function(){
+
     console.log("document ready");
+
+    let arr = new Array();
+
+    arr.push(3);
+    arr.push(5);
+    arr.push(7);
+    arr.push(8);
+
+    arr.display();
      
-    $("button").click(function(){
-       let box = document.createElement("div");
-       box.className = "box";
-       $(".container").append(box); 
-       console.log("box created..")
+    // $("button").click(function(){
+    //    let box = document.createElement("div");
+    //    box.className = "box";
+    //    $(".container").append(box); 
+    //    console.log("box created..")
+    // });
+
+    $("button").click(function() {
+        let box;
+
+        for (let i = 0; i < arr.length; i ++) {
+            box = document.createElement("div");
+            box.className = `box box-${arr.data[i]}`;
+            console.log(`box box-${arr.data[i]}`);
+            box.innerHTML = arr.data[i];
+
+            $(".container").append(box); 
+            // we only want to allow the user to make the array once 
+            $("button").prop('disabled',true);
+            
+        }
+        // if there reallly is a box3, its background will be pink.
+        console.log("this happens");
+        console.log("this also happens");
     });
 
-    let num = 6;
 
-    console.log(`number before the function call: ${num} `);
+   // let num = 6;
 
-    function myfunction(x)  {
-        console.log(x);
-        x = 7;
-        console.log(x);
+    // console.log(`number before the function call: ${num} `);
 
-        return x;
-    }
+    // function myfunction(x)  {
+    //     console.log(x);
+    //     x = 7;
+    //     console.log(x);
 
-        // let newNumber = myfunction(num);
+    //     return x;
+    // }
 
-    num = myfunction(num);
-    // num = newNumber;
+    //     // let newNumber = myfunction(num);
 
-    console.log(`number after the function call: ${num} `);
+    // num = myfunction(num);
+    // // num = newNumber;
+
+    // console.log(`number after the function call: ${num} `);
  
 
 });
