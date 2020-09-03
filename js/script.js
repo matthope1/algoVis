ce4feb4537a9aae2a1c58c6e330bb35d0dcee1f9
Matthew <matt-hope@hotmail.com>
2020-09-03 14:31:43 -0400

    fixed issue with the naming on array class

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-class Array {
+class array {
     constructor () {
         this.length = 0;
         this.data = [];
@@ -8,7 +8,7 @@ class Array {
         return data[index];
     }
 
-    push(value){
+    append(value){
         this.data.push(value);
         this.length++;
     }
@@ -18,6 +18,20 @@ class Array {
             arr.splice(index, 1);
         }
     }
+
+    linearSearch(value) {
+
+        let index = -1;
+
+        for (let i = 0; i < this.length; i++ ) {
+            if (this.data[i] == value) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
 
     display(){
         console.log(this.data);
@@ -32,21 +46,21 @@ $(document).ready(function(){
 
     console.log("document ready");
 
-    let arr = new Array();
+    let arr = new array();
 
-    arr.push(3);
-    arr.push(5);
-    arr.push(7);
-    arr.push(8);
+    arr.append(3);
+    arr.append(5);
+    arr.append(7);
+    arr.append(8);
 
-    arr.display();
-     
-    // $("button").click(function(){
-    //    let box = document.createElement("div");
-    //    box.className = "box";
-    //    $(".container").append(box); 
-    //    console.log("box created..")
-    // });
+    console.log("this happens");
+
+    $(".click-green").on('click', function() {
+
+        console.log("green's button was clicked...");
+        $(".green-box").css("background-color", "blue");
+    });
+
 
     $(".create_boxes").click(function() {
         let box;
@@ -54,53 +68,36 @@ $(document).ready(function(){
         for (let i = 0; i < arr.length; i ++) {
             box = document.createElement("div");
             box.className = `box box-${i}`;
-            // console.log(`box box-${arr.data[i]}`);
             box.innerHTML = arr.data[i];
 
-            $(".container").append(box); 
+            $(".c-one").append(box); 
             // we only want to allow the user to make the array once 
             $(".create_boxes").prop('disabled',true);
         }
+
     });
 
     $(".add_item").click(function () {
         let box;
 
+        //prompt user for the number to add
         let boxToBeAdded = prompt("what number to do you want to add to your array");
 
-        console.log("box to be added: " + boxToBeAdded);
-
+        //create the new div that will enclose the new array item
         box = document.createElement("div");
+
+        // add class name based on the current length of the array
         box.className = `box box-${arr.length}`;
 
+        // change the contents of the new div to the number given by the user
         box.innerHTML = boxToBeAdded;
 
-        $(".container").append(box);
+        // add div to container
+        $(".c-one").append(box);
 
-        arr.push(boxToBeAdded);
-        console.log("pushing item onto array..");
-        console.log(arr);
-    })
+        // add the new element to the storage array
+        arr.append(parseInt(boxToBeAdded));
 
 
-   // let num = 6;
-
-    // console.log(`number before the function call: ${num} `);
-
-    // function myfunction(x)  {
-    //     console.log(x);
-    //     x = 7;
-    //     console.log(x);
-
-    //     return x;
-    // }
-
-    //     // let newNumber = myfunction(num);
-
-    // num = myfunction(num);
-    // // num = newNumber;
-
-    // console.log(`number after the function call: ${num} `);
- 
-
+    });
 });
